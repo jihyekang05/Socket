a4207555b40c247be5ddd9b940e72991b429ea69
JIHYE KANG <62206966+jihyekang05@users.noreply.github.com>
2023-03-10 16:37:21 +0900

    Update MySocketClient.java

--- MySocketClient.java
+++ MySocketClient.java
@@ -1,57 +1,71 @@
 package NaverAPI;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class MySocketClient {
-		
+
+	// 내 ip 주소
+	public static String MessageBotHost = "내 ip주소입력";
+
+	// **수정예정
+	public static int MessageBotPort = 1629;
+
+
+	public static String MessageBotDelimeter = "#kisNaverBotSplit#";
+
+	// MessageBot.cs의 SendMessage함수
+	public static String SendMessage(String userList, String actCode, String sendTitle, String message,
+			String resultMsg, String Info) {
+
+		String cmd = null;
+		cmd = userList + MessageBotDelimeter + sendTitle + MessageBotDelimeter + message + MessageBotDelimeter
+				+ resultMsg + MessageBotDelimeter + actCode + MessageBotDelimeter + Info;
+
+		try {
+			Socket socket = new Socket(MessageBotHost, MessageBotPort);
+			System.out.println("소켓 서버 접속완료");
+
+			// Client -> Server로 전송
+			PrintWriter pr = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+
+			// 클라이언트가 보낸 메시지 출력
+			pr.println(cmd);
+
+			// 서버가 보낸 메시지 읽기
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+			// 서버로부터 온 메세지 확인
+			System.out.println(br.readLine());
+
+			// 소켓 종료
+			System.out.println("CLIENT SOCKET CLOSE");
+			socket.close();
+
+			return "OK : " + cmd;
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			System.out.println("NaverBot Server Error");
+			
+			return "ERROR : NaverBot Server Error";
+		}
+
+	}
+
 	public static void main(String[] args) {
 
-        Socket socket = null;
+		SendMessage("it_data@kispricing.com;", "3", "[TEST]", "로이터 IRS 금리 (16:00)", "확인", "인포");
 
-        try {
-            socket = new Socket();
-            System.out.println("[연결 요청]");
-            socket.connect(new InetSocketAddress("192.168.20.18", 5001));
-            System.out.println("[연결 성공]");
-           
-            byte[] bytes = null;
-            String message = null;
-
-            OutputStream os = socket.getOutputStream();
-            message = "Hello Server, I'm Client.";
-            bytes = message.getBytes("UTF-8");
-            os.write(bytes);
-            os.flush();
-            System.out.println("[데이터 보내기 성공]");
-           
-            InputStream is = socket.getInputStream();
-            bytes = new byte[100];
-            int readByteCount = is.read(bytes);
-            message = new String(bytes,0,readByteCount,"UTF-8");
-            System.out.println("[데이터 받기 성공] " + message);
-           
-            os.close();
-            is.close();
-           
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-       
-        if (!socket.isClosed()) {
-            try {
-                socket.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
+	}
 
 }
